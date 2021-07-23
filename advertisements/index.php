104f344e674cb5f45b8ed2cfa8f06cf60e40d415
ronanson <87083695+ronanson@users.noreply.github.com>
2021-07-23 10:03:34 +0200

    Add files via upload

---
+++ advertisements/index.php
@@ -0,0 +1,9 @@
+<?php include('view/header.php') ?>
+<?php include('view/navbar.php') ?>
+<?php
+    echo '<h1>Welcome! This is the starting page. To get started, choose one of the menu items!</h1>';
+?>
+
+
+
+
