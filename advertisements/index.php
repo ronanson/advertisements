81e1e1fca226801275ca344847f476908b47e256
ronanson <87083695+ronanson@users.noreply.github.com>
2021-07-23 10:12:35 +0200

    Delete advertisements directory

--- advertisements/index.php
+++
@@ -1,9 +0,0 @@
-<?php include('view/header.php') ?>
-<?php include('view/navbar.php') ?>
-<?php
-    echo '<h1>Welcome! This is the starting page. To get started, choose one of the menu items!</h1>';
-?>
-
-
-
-
